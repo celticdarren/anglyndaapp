a99284412b41d16d923cd4dd8b66bbcb073863f6
Darren Keen <dkeen@scottishpower.com>
2016-12-21 16:52:15 +0000

    commit

--- js/app.js
+++ js/app.js
@@ -2,6 +2,15 @@ var myApp = angular.module('myApp',
     ['ngRoute', 'firebase'])
     .constant('FIREBASE_URL', 'https://registerapp-bb5ea.firebaseio.com');
 
+myApp.run(['$rootScope', '$location', function($rootScope, $location) {
+    $rootScope.$on('$routeChangeError', function(event, next, previous, error) {
+        if(error == 'AUTH_REQUIRED') {
+            $rootScope.message = 'Sorry, must log in to access that page';
+            $location.path('/login');
+        }
+    });
+}]);
+
 myApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
     when('/login', {
@@ -14,7 +23,12 @@ myApp.config(['$routeProvider', function($routeProvider) {
     }).
     when('/success', {
         templateUrl: 'views/success.html',
-        controller: 'RegistrationController'
+        controller: 'RegistrationController',
+        resolve: {
+            currentAuthentication: function(Authentication) {
+                return Authentication.requireAuth();
+            } // current Auth
+        } // resolve
     }).
     otherwise({
         redirectTo: '/login'
--- js/services/authentication.js
+++ js/services/authentication.js
@@ -1,11 +1,37 @@
-myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', 'FIREBASE_URL', function($rootScope, $firebaseAuth, FIREBASE_URL) {
+myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', '$firebaseObject', '$location', 'FIREBASE_URL', function($rootScope, $firebaseAuth, $firebaseObject, $location, FIREBASE_URL) {
     var ref = firebase.database().ref();
     var auth = $firebaseAuth();
+
+    auth.$onAuthStateChanged(function(authUser) {
+        if (authUser) {
+            var userRef = firebase.database().ref('users/' + authUser.uid);
+            var userObj = $firebaseObject(userRef);
+            $rootScope.currentUser = userObj;
+        } else {
+            $rootScope.currentUser = '';
+        }
+    });
     
-    return {
+    var myObject = {
         login: function(user) {
-            $rootScope.message = "Welcome " + $rootScope.user.email;
+
+            auth.$signInWithEmailAndPassword(user.email, user.password)
+            .then(function(regUser) {
+                $location.path('/success');
+            }).catch(function(error) {
+                $rootScope.message = error.message;
+            }); // password login
+
         }, // login
+
+        logout: function() {
+            return auth.$signOut();
+        }, // logout
+
+        requireAuth: function() {
+            return auth.$requireSignIn();
+        },// requireAuth
+
         register: function(user) {
 
             auth.$createUserWithEmailAndPassword(user.email, user.password)
@@ -19,10 +45,13 @@ myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', 'FIREBASE_URL', 
                     email: user.email
                 });//user info
 
-                $rootScope.message = "Hi " + user.firstname + ", Thanks for registering";
+                myObject.login(user);
+
             }).catch(function(error) {
                 $rootScope.message = error.message; 
             }); // Create user
         } // register
-    }
+    };
+
+    return myObject;
 }]); // factory
